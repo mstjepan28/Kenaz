4aad85b623b675e1d1165d3eb5a148d869fa0dd7
mstjepan28 <55623249+mstjepan28@users.noreply.github.com>
2022-01-18 14:28:33 +0100

    Navbar

---
+++ src/components/navbar.vue
@@ -0,0 +1,140 @@
+<template>
+    <nav class="navbar">
+
+        <div class="navbar_top_wrapper">
+            <ul class="navbar_top_content">
+                <li class="wrapper_logo">
+                    <img src="@\assets\logo_white.png" alt="Kenaz logo">
+                </li>
+                <li class="title">Kenaz</li> 
+
+                <li class="break_element">Media</li> 
+                <li>Marketing</li> 
+                <li>Contact</li>
+
+                <li class="search">
+                    <img src="@\assets\search.png" alt="search icon">
+                </li>
+            </ul>
+        </div>
+
+        <div class="navbar_bottom_wrapper">
+            <ul class="navbar_bottom_content">
+                <NavbarCategory :key="category.category" :category="category" v-for="category in categoryList" />
+            </ul>
+        </div>
+
+    </nav>
+</template>
+
+<script>
+import NavbarCategory from "./navbarCategory.vue";
+
+export default {
+    component: { NavbarCategory },
+    data() {
+        return {
+            categoryList: [
+                { category: "NEWS", color: "#299EC3", newsList: [] },
+                { category: "BUSINESS", color: "#EE6151", newsList: [] },
+                { category: "SPORT", color: "#84C14F", newsList: [] },
+                { category: "LIFE", color: "#5DCFF3", newsList: [] },
+                { category: "TECH", color: "#FCC44D", newsList: [] },
+                { category: "TRAVEL", color: "#A99765", newsList: [] },
+            ]
+        };
+    },
+    components: { NavbarCategory }
+}
+</script>
+
+<style lang="scss">
+@import "@/styles/main.scss";
+
+.navbar{
+    width: 100%;
+}
+
+.navbar_top_wrapper,
+.navbar_bottom_wrapper{
+    display: flex;
+    justify-content: center;
+}
+
+.navbar_top_content,
+.navbar_bottom_content{
+    max-width: $content_width;
+    width: 100%;
+
+    list-style-type: none;
+
+    display: flex;
+
+}
+
+.navbar_top_wrapper{
+    background-color: $secondary;
+    
+    & > .navbar_top_content{
+        display: flex;
+
+        & > li{
+            display: flex;
+            align-items: center;
+
+            font-family: 'Varela Round', sans-serif;
+            font-style: normal;
+            font-weight: normal;
+            font-size: 14px;
+    
+            color: $font_color;
+    
+            padding: 12px 8px;
+        }
+
+        & > .wrapper_logo{
+            padding: 16px 24px;
+            background-color: $secondary-light; 
+
+            & > img{
+                width: 14px;
+                height: 21px;
+            }
+        }
+
+        & > .title{
+            font-family: Bitter;
+            font-style: normal;
+            font-weight: normal;
+            font-size: 24px;
+            line-height: 27px;
+
+            padding: 16px 8px 8px 8px;
+        }
+
+        & > .break_element{
+            margin-left: auto;
+        }
+
+        & > .search{
+            max-height: 56px;
+
+            padding: 20px 24px;
+            background-color: $secondary-light; 
+            
+            img{
+                width: 16px;
+                height: 16px;
+            }
+        }
+    }
+
+}
+
+.navbar_bottom_wrapper{
+    background-color: $primary;
+}
+
+
+
+</style>
---
+++ src/components/navbarCategory.vue
@@ -0,0 +1,83 @@
+<template>
+    <li class="category" :style="`border-bottom: 3px solid ${category.color}`">
+        <span class="category_title">{{category.category}}</span>
+        <div 
+            class="category_background" 
+            :style="`background-color: ${category.color}`"
+        ></div>
+    </li>
+</template>
+
+<script>
+
+export default {
+    props:{
+        category: String
+    }
+}
+</script>
+
+<style lang="scss">
+@import "@/styles/main.scss";
+
+.category{
+    padding: 16px 20px;
+
+    cursor: pointer;
+
+    position: relative;
+    overflow: hidden;
+
+    border-bottom: 3px solid red;
+
+    & > .category_title{
+        font-family: Bitter, sans-serif;
+        font-weight: 700;
+        font-size: 18px;
+        line-height: 21.6px;
+    
+        color: $font_color;
+
+        position: relative;
+
+        z-index: 2;
+    }
+
+    & > .category_background{
+        width: 100%;
+        height: 100px;
+
+        position: absolute;
+        top: 97px;
+        left: 0;
+        right: 0;
+
+        z-index: 1;
+    }
+
+    &:hover{
+        animation: removeBorder 0.4s forwards;
+
+        & > .category_background{
+            animation: moveUp 0.4s forwards;
+        }
+    }
+
+    @keyframes removeBorder {
+        from{
+            border-bottom-width: 3px;
+        }
+        to{
+            border-bottom-width: 0;
+        }
+    }
+    @keyframes moveUp {
+        from{
+            top: 97px;
+        }
+        to{
+            top: 0px;
+        }
+    }
+}
+</style>
