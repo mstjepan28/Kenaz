35dd83cb6de815df8ed22cf4e97ad7d38648c4a4
mstjepan28 <55623249+mstjepan28@users.noreply.github.com>
2022-01-19 19:24:17 +0100

    News carousel component

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,8 @@
 
 		<Banner :imgSrc="''"/>
 
+		<NewsCarousel :newsList="newsList"/>
+
 		<router-view/>
 	</div>
 </template>
@@ -11,9 +13,10 @@
 <script>
 import Navbar from "@/components/navbar.vue";
 import Banner from "./components/banner.vue";
+import NewsCarousel from "./components/newsCarousel.vue";
 
 export default {
-	components: { Navbar, Banner },
+	components: { Navbar, Banner, NewsCarousel },
 	data(){
 		return{
 			categoryList: [
@@ -23,7 +26,16 @@ export default {
                 { category: "LIFE", color: "#5DCFF3", newsList: [] },
                 { category: "TECH", color: "#FCC44D", newsList: [] },
                 { category: "TRAVEL", color: "#A99765", newsList: [] },
-            ]
+            ],
+
+			newsList: [
+				{id: 1, title: "'1 item Margot' breathlessly reimagines Anne Fran's sister", date: 1642616107134, comments: 21,  imgURL: "https://images.unsplash.com/photo-1642425146609-6c8ff4589d18?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2832&q=80"},
+				{id: 2, title: "'2 item Margot' breathlessly reimagines Anne Fran's sister", date: 1642616107134, comments: 22,  imgURL: "https://images.unsplash.com/photo-1642590044683-d72bfab9eb35?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80"},
+				{id: 3, title: "'3 item Margot' breathlessly reimagines Anne Fran's sister", date: 1642616107134, comments: 23,  imgURL: "https://images.unsplash.com/photo-1593642532842-98d0fd5ebc1a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2369&q=80"},
+				{id: 4, title: "'4 item Margot' breathlessly reimagines Anne Fran's sister", date: 1642616107134, comments: 24,  imgURL: "https://images.unsplash.com/photo-1642548666500-7990b88e691f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2778&q=80"},
+				{id: 5, title: "'5 item Margot' breathlessly reimagines Anne Fran's sister", date: 1642616107134, comments: 25,  imgURL: "https://images.unsplash.com/photo-1638913665258-ddd2bceafb30?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2370&q=80"},
+				{id: 6, title: "'6 item Margot' breathlessly reimagines Anne Fran's sister", date: 1642616107134, comments: 26,  imgURL: "https://images.unsplash.com/photo-1642524859252-448170eb1b11?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2369&q=80"},
+			]
 		}
 	}
 }
@@ -35,6 +47,7 @@ export default {
 
 .app{
 	width: 100%;
+	min-height: 100vh;
 
 	display: flex;
 	flex-direction: column;
---
+++ src/components/newsCarousel.vue
@@ -0,0 +1,227 @@
+<template>
+<div class="carousel" v-if="carouselContent">
+    <div class="carousel_controls">
+        <button class="carousel_controls_left" type="button" @click="changeSlide(-1)">
+            <img src="@/assets/svg/largeChevronLeft.svg" alt="left arrow">
+        </button>
+        <button class="carousel_controls_right" type="button" @click="changeSlide(1)">
+            <img src="@/assets/svg/largeChevronRight.svg" alt="right arrow">
+        </button>
+    </div>
+
+    <div class="carousel_description">
+        <div class="carousel_description_info">
+            <span class="carousel_description_info_date">{{formattedDate}}</span>
+            <span class="carousel_description_info_comments"> <img src="@/assets/commentBubble.png" alt="comment bubble">{{carouselContent[selectedIndex].comments}} Comments</span>
+        </div>
+
+        <h1 class="carousel_description_title">{{carouselContent[selectedIndex].title}}</h1>
+
+        <button class="carousel_description_gotoArticle">Read article</button>
+    </div>
+
+    <div class="carousel_background">
+        <img :key="slide.id" :src="slide.imgURL" v-for="slide in carouselContent">
+    </div>
+</div>
+</template>
+
+<script>
+import dayjs from 'dayjs';
+
+export default {
+    props:{
+        newsList:{
+            type: Array,
+            required: true
+        }
+    },
+    data(){
+        return{
+            selectedIndex: 0,
+            carouselContent: null,
+
+            carouselOffset: 0,
+            isTransitioning: false,
+        }
+    },
+    computed:{
+        formattedDate(){
+            const date = this.carouselContent[this.selectedIndex].date
+            return dayjs(date).format('MMMM DD, YYYY');
+        }
+    },
+    methods: {
+        // Adds/subtracts from the current index and makes sure the index stays in bounds 
+        changeSlide(direction){
+            if(this.isTransitioning) return;
+            this.isTransitioning = true
+
+            this.selectedIndex += direction;
+
+            if(this.selectedIndex >= this.carouselContent.length)
+                this.selectedIndex = 0;
+            else if(this.selectedIndex < 0)
+                this.selectedIndex = this.carouselContent.length - 1
+
+            this.moveImages();
+
+            setTimeout(() => {
+                this.isTransitioning = false
+            }, 750)
+        },
+
+        // On button click, change move the carousel
+        moveImages(){
+            const carousel = document.querySelector("div.carousel_background");
+            
+            this.carouselOffset = this.selectedIndex * -carousel.offsetWidth;
+            
+            carousel.style.transition = "0.75s ease-in-out";
+            carousel.style.transform = `translateX(${this.carouselOffset}px)`;
+
+            setTimeout(() => {
+                this.checkForReset(carousel)
+            }, 750)
+        },
+
+        // Check if the last/first slide is reached, if it is, turn off the transition and reset the slide
+        checkForReset(carousel){
+            const maxOffset = (carousel.children.length - 1) * carousel.offsetWidth
+            
+            if(Math.abs(this.carouselOffset) == maxOffset){
+                this.selectedIndex = 1;
+                this.resetCarousel(carousel);
+            }
+            else if(this.carouselOffset == 0){
+                this.selectedIndex = carousel.children.length - 2;
+                this.resetCarousel(carousel);
+            }
+        },
+        resetCarousel(carousel){
+            this.carouselOffset = this.selectedIndex * -carousel.offsetWidth;
+
+            carousel.style.transition = "none";
+            carousel.style.transform = `translateX(${this.carouselOffset}px)`;
+        },
+
+        // Clones the first and last element to the opposite place in the list so 
+        // we can create the infinite effect
+        cloneElements(){
+            const firstElem = this.newsList[0];
+            const lastElem = this.newsList[this.newsList.length - 1];
+
+            this.carouselContent = [lastElem, ...this.newsList, firstElem];
+        }
+    },
+    mounted(){
+        this.cloneElements();
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+@import "@/styles/main.scss";
+
+@mixin fontStyle($font, $size, $weight, $lineHeight, $color){
+    font-family: $font;
+    font-size: $size;
+    font-weight: $weight;
+    line-height: $lineHeight;
+    color: $color;
+}
+
+.carousel{
+    width: 100%;
+    max-width: $content_width;
+    height: 425px;
+
+    margin-bottom: 20px;
+
+    position: relative;
+    overflow: hidden;
+
+    & > .carousel_controls{
+        width: 100%;
+
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        z-index: 2;
+
+        padding: 0 28px;
+
+        & > button{
+            cursor: pointer;
+            border: none;
+            background-color: transparent;
+        }
+    }
+
+    & > .carousel_description{
+        display: flex;
+        flex-direction: column;
+
+        margin: 28px;
+
+        position: absolute;
+        bottom: 0;
+        z-index: 2;
+
+        & > .carousel_description_info{
+            display: flex;
+            column-gap: 24px;
+
+            & > span{
+                @include fontStyle("Varela Round", 12px, 400, 14px, $font_color-gray);
+
+                display: flex;
+                align-items: center;
+            }
+
+            & img{
+                width: 16px;
+                height: 14px;
+
+                margin-right: 8px;
+            }
+        }
+
+        & > .carousel_description_title{
+            @include fontStyle("Bitter", 34px, 700, 41px, $font_color-gray);
+            max-width: 65%;
+
+            margin: 12px 0;
+        }
+
+        & > .carousel_description_gotoArticle{
+            @include fontStyle("Varela Round", 14px, normal, 17px, $font_color-gray);
+
+            align-self: flex-start;
+
+            padding: 8px 22px;
+
+            cursor: pointer;
+
+            border: 1px solid #FFFFFF;
+            background-color: transparent;
+        }
+    }
+
+    & > .carousel_background{
+        min-width: 100%;
+        height: 100%;
+
+        display: flex;
+
+        & > img{
+            min-width: 100%;
+            height: 100%;
+        }
+    }
+}
+</style>
