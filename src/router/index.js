0651539a298f4c848f512999224741c629c5f227
mstjepan28 <55623249+mstjepan28@users.noreply.github.com>
2022-02-03 13:54:09 +0100

    Scorll behaviour on route change

--- src/router/index.js
+++ src/router/index.js
@@ -23,7 +23,10 @@ const routes = [
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
+  routes,
+  scrollBehavior (to, from, savedPosition) {
+    return {left: 0, top: 0}
+  }
 })
 
 export default router
